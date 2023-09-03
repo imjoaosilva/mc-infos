b6efd23c98b0332a1042966a988db5a298b925f6
Z4 DEV <z4kreyyt@gmail.com>
2023-09-03 19:23:44 +0100

    head 2d route

--- src/routes/v1/preview/mod.rs
+++ src/routes/v1/preview/mod.rs
@@ -1,4 +1,6 @@
 mod cape;
+mod head2d;
+mod head3d;
 
 use actix_web::web;
 
@@ -6,5 +8,7 @@ pub fn config(cfg: &mut web::ServiceConfig) {
   cfg.service(
     web::scope("/preview")
     .route("/cape/{username}", web::get().to(cape::get_cape))
+    .route("/2dhead/{username}", web::get().to(head2d::get_2dhead))
+    .route("/head3d/{username}", web::get().to(head3d::get_3dhead))
   );
 }
--- src/services/preview_service.rs
+++ src/services/preview_service.rs
@@ -10,3 +10,23 @@ pub fn cape_manipulation(img: Vec<u8>) -> Vec<u8> {
 
     return bytes;
 }
+
+pub fn head2d_manipulation(img: Vec<u8>) -> Vec<u8> {
+    let imgdata = image::load_from_memory(&img).unwrap();
+    let mut zoomed = imgdata.resize(512, 512, image::imageops::FilterType::Nearest);
+    let cutted = zoomed.crop(65, 65, 65, 63);
+    let mut bytes: Vec<u8> = Vec::new();
+    cutted.write_to(&mut Cursor::new(&mut bytes), image::ImageOutputFormat::Png).unwrap();
+
+    return bytes;
+}
+
+pub fn head3d_manipulation(img: Vec<u8>) -> Vec<u8> {
+    let imgdata = image::load_from_memory(&img).unwrap();
+    let mut zoomed = imgdata.resize(512, 512, image::imageops::FilterType::Nearest);
+    let cutted = zoomed.crop(65, 65, 65, 63);
+    let mut bytes: Vec<u8> = Vec::new();
+    cutted.write_to(&mut Cursor::new(&mut bytes), image::ImageOutputFormat::Png).unwrap();
+
+    return bytes;
+}
